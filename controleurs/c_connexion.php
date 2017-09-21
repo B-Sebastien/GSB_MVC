f543e852748fa0b447d00812e49b91f6e5e08a31
B-Sebastien <sebastien.borg28@gmail.com>
2017-09-21 16:42:04 +0200

    Modification CSS | Ajout d'une fonction infoscomptable dans la class pdo | Ajout des infos d'un comptable dans le controleur connexion

--- controleurs/c_connexion.php
+++ controleurs/c_connexion.php
@@ -1,6 +1,6 @@
-﻿<?php
-if(!isset($_REQUEST['action'])){
-	$_REQUEST['action'] = 'demandeConnexion';
+<?php
+if(!isset($_REQUEST['action'])){ 
+	$_REQUEST['action'] = 'demandeConnexion'; 
 }
 $action = $_REQUEST['action'];
 switch($action){
@@ -11,19 +11,34 @@ switch($action){
 	case 'valideConnexion':{
 		$login = $_REQUEST['login'];
 		$mdp = $_REQUEST['mdp'];
+		$comptable = $pdo->getInfosComptable($login,$mdp); /*création d'un variable comptable*/
 		$visiteur = $pdo->getInfosVisiteur($login,$mdp);
-		if(!is_array( $visiteur)){
-			ajouterErreur("Login ou mot de passe incorrect");
+		
+		if ( ! is_array($comptable) && ! is_array($visiteur)){ /* si les infos de visiteur ou comptable ne sont pas des tableaux*/
+			ajouterErreur("Login ou mot de passe incorrect !");
 			include("vues/v_erreurs.php");
 			include("vues/v_connexion.php");
 		}
-		else{
+		else if (is_array($visiteur)){
 			$id = $visiteur['id'];
 			$nom =  $visiteur['nom'];
 			$prenom = $visiteur['prenom'];
 			connecter($id,$nom,$prenom);
 			include("vues/v_sommaire.php");
 		}
+			else{
+			$id = $comptable['id'];
+			$nom =  $comptable['nom'];
+			$prenom = $comptable['prenom'];
+			connecter($id,$nom,$prenom);
+			include("vues/v_sommaire.php");
+			}
+		
+		break;
+        }
+        case'deconnexion':{
+            deconnecter();
+            include("vues/v_connexion.php");
 		break;
 	}
 	default :{
