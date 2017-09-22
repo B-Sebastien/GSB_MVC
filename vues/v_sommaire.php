b5e9b7cde17062266be9d2ca183ce3cd8f411743
Sébastien <Sbastien@192.168.1.10>
2017-09-22 23:28:18 +0200

    Modification de la vue sommaire pour différencier un (comptable/visiteur)

--- vues/v_sommaire.php
+++ vues/v_sommaire.php
@@ -1,27 +1,38 @@
-    <!-- Division pour le sommaire -->
-    <div id="menuGauche">
-     <div id="infosUtil">
-    
-        <h2>
-    
-</h2>
-    
-      </div>  
-        <ul id="menuList">
-			<li >
-				  Visiteur :<br>
-				<?php echo $_SESSION['prenom']."  ".$_SESSION['nom']  ?>
-			</li>
-           <li class="smenu">
+<!-- Division pour le sommaire -->
+<div id="menuGauche">
+    <div id="infosUtil">
+
+        <h2></h2>
+
+    </div>  
+    <ul id="menuList">
+        <li>
+            Bienvenue <?php echo $_SESSION['prenom'] . " ". $_SESSION['nom'] ?> (<?php echo $_SESSION['type']?>) !
+        <li><br>
+            <!---------------------------------------------- AFFICHAGE VISITEUR ---------------------------------------------->
+            <?php if ($_SESSION['type'] == "visiteur"): ?>
+	</li>
+        <li class="smenu">
               <a href="index.php?uc=gererFrais&action=saisirFrais" title="Saisie fiche de frais ">Saisie fiche de frais</a>
-           </li>
-           <li class="smenu">
-              <a href="index.php?uc=etatFrais&action=selectionnerMois" title="Consultation de mes fiches de frais">Mes fiches de frais</a>
-           </li>
- 	   <li class="smenu">
-              <a href="index.php?uc=connexion&action=deconnexion" title="Se déconnecter">Déconnexion</a>
-           </li>
+        </li>
+        <li class="smenu">
+            <a href="index.php?uc=etatFrais&action=selectionnerMois" title="Consultation de mes fiches de frais">Mes fiches de frais</a>
+        </li>
+        <?php endif; ?>
+        
+        <!---------------------------------------------- AFFICHAGE COMPTABLE ---------------------------------------------->
+        <!-------------------- Pour le moment, le comptable à les mêmes liens que le visiteur -------------------->
+        <?php if ($_SESSION['type'] == "comptable"): ?>
+        <li class="smenu">
+              <a href="index.php?uc=gererFrais&action=saisirFrais" title="Saisie fiche de frais ">Saisie fiche de frais</a>
+        </li>
+        <li class="smenu">
+            <a href="index.php?uc=etatFrais&action=selectionnerMois" title="Consultation de mes fiches de frais">Mes fiches de frais</a>
+        </li>
+        <?php endif; ?>
+        
+ 	<li class="smenu">
+            <a href="index.php?uc=connexion&action=deconnexion" title="Se déconnecter">Déconnexion</a>
+        </li>
          </ul>
-        
-        
     </div>
