a95903b483bd25f0c99bdbfcfc7799ce40110ea9
B-Sebastien <sebastien.borg28@gmail.com>
2017-11-03 14:37:28 +0100

    Ajout des liens report/refus/valider frais

--- vues/v_visiteurFrais.php
+++ vues/v_visiteurFrais.php
@@ -1,11 +1,35 @@
 <?php // var_dump($leMois) ?>
 <div name="bas">
+<!--    <table class="listeLegere">
+        <caption>Eléments forfaitisés </caption>
+        <tr>
+            <?php
+            foreach ($lesFraisForfait as $unFraisForfait) {
+                $libelle = $unFraisForfait['libelle'];
+                ?>	
+                <th> <?php echo $libelle ?></th>
+                <?php }
+            ?>
+        </tr>
+
+        <tr>
+            <?php
+            foreach ($lesFraisForfait as $unFraisForfait) {
+                $quantite = $unFraisForfait['quantite'];
+                ?>
+                <td class="qteForfait"><?php echo $quantite ?> </td>
+                <?php
+            }
+            ?>
+        </tr>
+    </table>-->
         <table class="listeLegere">
             <h3>Descriptif des éléments hors forfait</h3>
             <tr>
                 <th class="date">Date</th>
                 <th class="libelle">Libellé</th>  
-                <th class="montant">Montant</th>
+                <th class="montant">Montant</th> 
+                <th class="libelle">Situation</th>
                 <th class="action">&nbsp;</th>    
                 <th class="libelle"> </th>
                 <th class="action"> </th> 
@@ -16,12 +40,20 @@
                 $libelle = $unFraisHorsForfait['libelle'];
                 $date = $unFraisHorsForfait['date'];
                 $montant = $unFraisHorsForfait['montant'];
+                $idFrais = $unFraisHorsForfait['id'];
+                $situation = $pdo->voirFraisRefuse($idFrais);
                 ?>		
                 <tr>
                     <td> <?php echo $date ?></td>
                     <td><?php echo $libelle ?></td>
                     <td><?php echo $montant ?></td>
-                    <td><a href="index.php?uc=fraisAValider&action=supprimerFrais&idFrais=<?php echo $idFrais ?>"onclick="return confirm('Voulez-vous vraiment supprimer ce frais?');">Supprimer ce frais</a></td>
+                    <td><?php echo $situation[0][0] ?></td>
+                    <!--<td><a href="index.php?uc=fraisAValider&action=supprimerFrais&idFrais=<?php echo $idFrais ?>"onclick="return confirm('Voulez-vous vraiment supprimer ce frais?');">Supprimer ce frais</a></td>-->
+               
+                    <td><a href="index.php?uc=fraisAValider&action=refuserFrais&idFrais=<?php echo $idFrais ?>"onclick="return confirm('Voulez-vous vraiment refuser ce frais?');">Refuser ce frais</a></td>
+                    <td><a href="index.php?uc=fraisAValider&action=reporterFrais&idFrais=<?php echo $idFrais ?>"onclick="return confirm('Voulez-vous vraiment reporter ce frais?');">Reporter ce frais</a></td>
+                    <td><a href="index.php?uc=fraisAValider&action=validerFraisHorsForfait&idFrais=<?php echo $idFrais ?>"onclick="return confirm('Voulez-vous vraiment valider ce frais?');">Valider ce frais</a></td>
+                
                 </tr>
                 <?php
             }
